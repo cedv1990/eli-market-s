931e7634bc997e3b104de7dfa2dad41d59415312
S5 <carlos.diaz@sinco.com.co>
2018-03-02 12:46:19 -0500

    Se quitan rutas innecesarias
    Se quitan rutas innecesarias

--- routes/index.js
+++ routes/index.js
@@ -4,20 +4,14 @@ var router = express.Router();
 var test = require('../services/test');
 var super_market = require('../services/super_market');
 
-var committees = require('../services/committees');
 var migrations = require('../services/migrations');
-var menu = require('../services/menu');
 
 router.get('/test', test.ping);
-router.get('/super', super_market.getAll);
-
 
 router.get('/migrations/drop', migrations.drop);
 router.get('/migrations/create', migrations.create);
 router.get('/migrations/insert', migrations.insert);
-router.get('/committees', committees.getAll);
-router.get('/committees/:cod', committees.getSingle);
-router.get('/committees/:cod/index', committees.getIndexContent);
-router.get('/committees/:cod/menu', menu.getMenu);
+
+router.get('/super', super_market.getAll);
 
 module.exports = router;
