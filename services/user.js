92a288e738816775bdec825d3293f81aee9d22c6
S5 <carlos.diaz@sinco.com.co>
2018-03-07 15:17:54 -0500

    a
    a

--- services/user.js
+++ services/user.js
@@ -4,7 +4,7 @@ var s5 = require('s5-js/s5');
 var validate = function(req, res, next){
   var user = JSON.parse( req.body.user );
 
-  db.database().any('select cod_user_market from user_market where (alias_user_market = $1 or email_user_market = $1) and pass_user_market = $2 and state_user = 1', [user.usuario, s5.toAESEncrypt(s5.fromAESEncrypt(user.pass), true)])
+  db.database().any('select cod_user_market from user_market where (alias_user_market = $1 or email_user_market = $1) and pass_user_market = $2 and state_user = true', [user.usuario, s5.toAESEncrypt(s5.fromAESEncrypt(user.pass), true)])
     .then(function (data) {
       res.status(200)
         .json({
