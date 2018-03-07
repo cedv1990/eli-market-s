69874e5ab81b6ff50ac6026cec55973372a318c2
S5 <carlos.diaz@sinco.com.co>
2018-03-07 15:28:14 -0500

    A
    A

--- services/user.js
+++ services/user.js
@@ -4,12 +4,12 @@ var s5 = require('s5-js/s5');
 var validate = function(req, res, next){
   var user = JSON.parse( req.body.user );
 
-  db.database().any('select cod_user_market from user_market where (alias_user_market = $1 or email_user_market = $1) and pass_user_market = $2 and state_user = true', [user.usuario, s5.toAESEncrypt(s5.fromAESEncrypt(user.pass), true)])
+  db.database().any('select cod_user_market, name_user_market, last_name_user_market, alias_user_market, email_user_market from user_market where (alias_user_market = $1 or email_user_market = $1) and pass_user_market = $2 and state_user = true', [user.usuario, s5.toAESEncrypt(s5.fromAESEncrypt(user.pass), true)])
     .then(function (data) {
       res.status(200)
         .json({
           status: 'success',
-          data: data,
+          data: data.shift(),
           message: 'Retorna si usuario existe'
         });
     })
